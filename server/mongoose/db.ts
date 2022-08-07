4db749f69a83994851fe6479d3d63910eade13f9
yusuf <yusufanka54@gmail.com>
2022-08-07 16:36:03 +0100

    yes

--- server/mongoose/db.ts
+++ server/mongoose/db.ts
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
-import { port } from '../../bin/www'
 
 const connectDB = async () => {
     try {
         console.log('Here:',process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB connected to ${port}`)
+        
 
     } catch(error) {
         console.error(error)
